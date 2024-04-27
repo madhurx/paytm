cbabd407489bf865f3f2ef0866a209ad4f07848b
Madhur <madhurrgupta@gmail.com>
2024-04-28 00:02:09 +0530

    feat: changes

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -7,21 +7,25 @@ import { AppbarClient } from "../components/AppbarClient";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-	title: "Wallet",
-	description: "Simple wallet app",
+  title: "Wallet",
+  description: "Simple wallet app",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }): JSX.Element {
-	return (
-		<html lang="en">
-			<Providers>
-				<AppbarClient />
-				<body className={inter.className}>{children}</body>
-			</Providers>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <Providers>
+        <body className={inter.className}>
+          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
+            <AppbarClient />
+            {children}
+          </div>
+        </body>
+      </Providers>
+    </html>
+  );
 }
